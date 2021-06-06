8e6a947361c0030e2cf2c2686e7a59d9037aac57
NhatQuangLN <nhatquangfptu@gmail.com>
2021-06-06 17:32:33 +0700

    add game find logo dever

---
+++ js/giai_do_phan1.js
@@ -0,0 +1,8 @@
+function methodFalse(){
+    alert("Not this picture! Try again!");
+}
+function methodTrue(){
+    document.getElementById("show2").innerHTML="The answer is: 4";
+    document.getElementById("show3").innerHTML="Hurry up to comment to get the prized!";
+    document.getElementById("show").style.display="block";
+}
