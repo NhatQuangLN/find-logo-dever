8361d53e2fde64f280e62e9cdb8287e8e5cdd54c
NhatQuangLN <74296186+NhatQuangLN@users.noreply.github.com>
2021-06-10 22:57:48 +0700

    Update giai_do_phan1.js

--- js/giai_do_phan1.js
+++ js/giai_do_phan1.js
@@ -3,6 +3,6 @@ function methodFalse(){
 }
 function methodTrue(){
     document.getElementById("show2").innerHTML="The answer is: 4";
-    document.getElementById("show3").innerHTML="Hurry up to comment to get the prized!";
+    document.getElementById("show3").innerHTML="Captrue this and Hurry up to comment to get the prized!";
     document.getElementById("show").style.display="block";
 }
