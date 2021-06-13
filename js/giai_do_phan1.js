77dcc3bbd1942a3d14bcc07959bd3a068ec32a94
NhatQuangLN <74296186+NhatQuangLN@users.noreply.github.com>
2021-06-13 18:35:36 +0700

    Update giai_do_phan1.js

--- js/giai_do_phan1.js
+++ js/giai_do_phan1.js
@@ -2,6 +2,8 @@ function methodFalse(){
     alert("Not this picture! Try again!");
 }
 function methodTrue(){
+    var name = prompt("Your name is: ");
+    documnet.getElementById("showten").innerHTML="Hello " + name;
     document.getElementById("show2").innerHTML="The answer is: 4";
     document.getElementById("show3").innerHTML="Captrue this and Hurry up to comment to get the prized!";
     document.getElementById("show").style.display="block";
